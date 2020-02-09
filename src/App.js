2607d909b8caf0aca2d21dfeda5e5de02a79f2b1
Cotton Top <mtmcgr@gmail.com>
2020-02-08 20:50:01 -0600

    file structure and readme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Employee Directory</h1>
+        </header>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* <Card /> */}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
